7411b04bcdae24b197d20dc267c58ea7e64245b1
Yondemon4266 <Alisan4299@gmail.com>
2023-09-11 15:45:29 +0200

    memd

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -39,6 +39,5 @@ module.exports.signIn = async (req, res) => {
 module.exports.logout = async (req, res) => {
     res.cookie('jwt', '', { maxAge: 1, sameSite: "None", secure: true});
     console.log('déconnecté');
-    res.redirect('/');
-}
+};
 
