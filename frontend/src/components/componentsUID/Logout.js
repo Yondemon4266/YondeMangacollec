4c17126127f2924080fbfc73bc90d62ebd137ef6
Yondemon4266 <Alisan4299@gmail.com>
2023-09-11 15:29:26 +0200

    slk

--- frontend/src/components/componentsUID/Logout.js
+++ frontend/src/components/componentsUID/Logout.js
@@ -11,6 +11,7 @@ const Logout = () => {
   };
 
   const logout = async () => {
+    localStorage.removeItem("uid");
     await axios({
       method: "get",
       url: `${process.env.REACT_APP_API_URL}api/user/logout`,
@@ -18,7 +19,6 @@ const Logout = () => {
     })
       .then(() => {
         removeCookie("jwt");
-        localStorage.removeItem("uid");
         window.location = "/";
       })
       .catch((err) => console.log(err));
