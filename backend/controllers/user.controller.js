0039d5426b29aff079f12b89a3318ff2506b8d54
Yondemon4266 <Alisan4299@gmail.com>
2023-12-15 18:34:22 +0100

    add

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -263,18 +263,39 @@ module.exports.userPseudoChange = async (req, res) => {
 };
 
 module.exports.userPasswordChange = async (req, res) => {
-  if (!ObjectID.isValid(req.params.id))
+  if (!ObjectID.isValid(req.params.id)) {
     return res.status(400).send("Id non reconnu : " + req.params.id);
+  }
+
   try {
     const user = await UserModel.findById(req.params.id);
-    if (!user)
-      return res.status(404).json({ message: "Utilisateur non trouvé" });
 
-    user.password = req.body.password;
-    await user.save();
-    return res
-      .status(200)
-      .json({ message: "Mot de passe mis à jour avec succès"});
+    if (!user) {
+      return res.status(404).json({ message: "Utilisateur non trouvé" });
+    }
+
+    // Vérifier que le nouveau mot de passe est différent de l'ancien
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      return res.status(400).json({
+        message: "Le nouveau mot de passe doit être différent de l'ancien",
+      });
+    }
+    if (await bcrypt.compare(req.body.oldpassword, user.password)) {
+      // Hasher le nouveau mot de passe
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+      // Mettre à jour le mot de passe haché dans la base de données
+      user.password = hashedPassword;
+      await user.save();
+
+      return res
+        .status(200)
+        .json({ message: "Mot de passe mis à jour avec succès", token });
+    } else {
+      return res
+        .status(400)
+        .json({ message: "L'ancien mot de passe n'est pas valide" });
+    }
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: "Erreur serveur", err });
