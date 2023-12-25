1ffbcb495c9bfebcade48cdb532a7b4b781cb99c
Yondemon4266 <Alisan4299@gmail.com>
2023-12-25 02:36:09 +0100

    change

--- frontend/src/components/componentsUID/DeleteAccount.js
+++ frontend/src/components/componentsUID/DeleteAccount.js
@@ -14,36 +14,35 @@ const DeleteAccount = () => {
   };
 
   const DeleteAcc = async () => {
-    if (deletionInput === "SUPPRIMER") {
-      try {
-        localStorage.removeItem("uid");
-        const response = await axios({
-          method: "get",
-          url: `${process.env.REACT_APP_API_URL}api/user/delete/${
-            userInfo && userInfo._id
-          }`,
-          withCredentials: true,
-        });
-        setMsgErrors((prev) => ({
-          ...prev,
-          error: "",
-          success: "Votre compte a été supprimé avec succès !",
-        }));
-        setTimeout(() => {
-          removeCookie("jwt");
-          window.location = "/";
-        }, 2000);
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
+    try {
+      localStorage.removeItem("uid");
+      const response = await axios({
+        method: "delete",
+        url: `${process.env.REACT_APP_API_URL}api/user/delete/${
+          userInfo && userInfo._id
+        }`,
+        withCredentials: true,
+      });
       setMsgErrors((prev) => ({
         ...prev,
-        error: "Tapez 'SUPPRIMER' en majuscule'",
+        error: "",
+        success: "Votre compte a été supprimé avec succès !",
+      }));
+      setTimeout(() => {
+        removeCookie("jwt");
+        console.log("ok");
+        window.location = "/";
+      }, 2000);
+    } catch (error) {
+      console.log(error);
+      setMsgErrors((prev) => ({
+        ...prev,
+        error: "Erreur serveur...",
         success: "",
       }));
     }
   };
+
   return (
     <>
       <div className="logout" onClick={() => setDeleteVisible(true)}>
@@ -62,10 +61,12 @@ const DeleteAccount = () => {
               <button type="button" onClick={() => setDeleteVisible(false)}>
                 Annuler
               </button>
-              <button type="button" onClick={DeleteAcc}>
+              <button type="button" onClick={() => DeleteAcc()}>
                 Confirmer
               </button>
             </div>
+            {msgErrors.error ? msgErrors.error : null};
+            {msgErrors.success ? msgErrors.success : null};
           </div>
         </div>
       )}
