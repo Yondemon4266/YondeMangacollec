08cb883388c7149e38e03038941c41bcf10d6b12
Yondemon4266 <Alisan4299@gmail.com>
2023-12-25 01:40:35 +0100

    change icon

--- frontend/src/components/LogoTitle.js
+++ frontend/src/components/LogoTitle.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LogoTitle = () => {
     return (
       <div className="logo-title">
-        <img src="./img/logo.webp" alt="logo" className="logo" />
+        <img src="./img/logo.png" alt="logo" className="logo" />
         <h1>Hinokuni Animes</h1>
       </div>
     );
